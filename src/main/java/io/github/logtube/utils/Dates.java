539a2d229bb29c5feb974c138836f98a0e252d04
Yanke Guo <guoyk@pagoda.com.cn>
2019-04-19 17:52:24 +0800

    utils: using ThreadLocal for SimpleDateFormat

--- src/main/java/io/github/logtube/utils/Dates.java
+++ src/main/java/io/github/logtube/utils/Dates.java
@@ -3,25 +3,19 @@ package io.github.logtube.utils;
 import org.jetbrains.annotations.NotNull;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 public class Dates {
 
-    private static final SimpleDateFormat LINE_TIMESTAMP_PREFIX_FORMAT = new SimpleDateFormat("[yyyy-MM-dd HH:mm:ss.SSS ZZZZZ]");
-
-    public static @NotNull Date yesterday() {
-        final Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DATE, -1);
-        return cal.getTime();
-    }
-
-    public static @NotNull String yesterday_yyyyMMdd() {
-        return new SimpleDateFormat("yyyyMMdd").format(yesterday());
-    }
+    private static final ThreadLocal<SimpleDateFormat> LINE_TIMESTAMP_FORMAT = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        public SimpleDateFormat get() {
+            return new SimpleDateFormat("[yyyy-MM-dd HH:mm:ss.SSS ZZZZZ]");
+        }
+    };
 
     public static @NotNull String formatLineTimestamp(long epoch) {
-        return LINE_TIMESTAMP_PREFIX_FORMAT.format(new Date(epoch));
+        return LINE_TIMESTAMP_FORMAT.get().format(new Date(epoch));
     }
 
 }
--- src/test/java/io/github/logtube/utils/DatesTest.java
+++ src/test/java/io/github/logtube/utils/DatesTest.java
@@ -2,22 +2,13 @@ package io.github.logtube.utils;
 
 import org.junit.Test;
 
-import java.util.Date;
-
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class DatesTest {
 
     @Test
-    public void yesterday() {
-        Date now = new Date();
-        Date yesterday = Dates.yesterday();
-        long diff = now.getTime() - yesterday.getTime();
-        long devi = Math.abs(diff - 60 * 60 * 24 * 1000);
-        assertTrue(devi < 500);
+    public void formatLineTimestamp() {
+        assertEquals("[2019-04-19 17:45:42.052 +0800]", Dates.formatLineTimestamp(1555667142052L));
     }
 
-    @Test
-    public void yesterday_yyyyMMdd() {
-    }
 }
